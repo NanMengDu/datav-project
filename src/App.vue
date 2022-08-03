a6c4b10529e9bfc6c9a27e43fc727fc47b43cd44
dmn <2464016555@qq.com>
2022-08-03 13:46:50 +0800

    封装表单组件

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,72 @@
       <dv-decoration-1 :color="['red', 'green']" />
     </div>
     <div class="dv">
-      <dv-decoration-2/>
+      <dv-decoration-2 />
     </div>
+    <!-- 组件 -->
+    <chris-el-table :table-title="tableTitle" :table-data="tableData">
+      <el-table-column slot="handle" label="操作" width="180">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)">查看</el-button>
+        </template>
+      </el-table-column>
+    </chris-el-table>
   </div>
 </template>
 
 <script>
+import ChrisElTable from "./components/chris-el-table.vue";
 export default {
   name: "App",
-  components: {},
+  components: { ChrisElTable },
+  data() {
+    return {
+      tableTitle: [
+        {
+          label: "日期",
+          property: "date",
+        },
+        {
+          label: "姓名",
+          property: "name",
+        },
+        {
+          label: "地址",
+          property: "address",
+          slot:'handle'
+        },
+      ],
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods:{
+    handleClick(row){
+        console.log(row);
+    }
+  }
 };
+
 </script>
 
 <style>
---
+++ src/components/chris-el-table.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <el-table :data="tableData" style="width: 100%">
+      <div v-for="(item, index) in tableTitle" :key="index">
+        <el-table-column :prop="item.property" :label="item.label" width="180">
+        </el-table-column>
+        <!-- 插槽 -->
+        <slot v-if="item.slot" name='handle'></slot>
+      </div>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "chris-el-table",
+  props: {
+    tableData: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+    tableTitle: {
+      type: Array,
+      require: true,
+    },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  margin-bottom: 20px;
+}
+/* 去掉表格底部边框 */
+.container .el-table::before {
+  background: none;
+}
+
+/* 表格hover效果 */
+::v-deep
+  .el-table--enable-row-hover
+  .el-table__body
+  tr:hover
+  > td.el-table__cell {
+  background-color: #a1b1c8;
+}
+/* 表头样式 */
+::v-deep .el-table__header-wrapper .el-table__cell {
+  height: 44px;
+  padding: 0;
+  background: #ded6d6;
+  border-bottom: #ebeef5 solid 1px !important;
+  text-align: center;
+}
+
+::v-deep .el-table__row .el-table__cell .cell{
+    display: flex;
+    justify-content: center;
+}
+</style>
